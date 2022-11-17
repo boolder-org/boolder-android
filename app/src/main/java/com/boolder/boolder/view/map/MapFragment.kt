80725d2ee682fe50c0139c8141e25c8233840fd8
Louis Gautier <Louis.gautier@outlook.fr>
2022-11-17 17:20:12 +0100

    first try on Layers + remove code

--- app/src/main/java/com/boolder/boolder/view/map/MapFragment.kt
+++ app/src/main/java/com/boolder/boolder/view/map/MapFragment.kt
@@ -12,7 +12,16 @@ import com.boolder.boolder.databinding.FragmentMapBinding
 import com.boolder.boolder.viewBinding
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
-import com.mapbox.maps.CameraOptions.Builder
+import com.mapbox.maps.extension.observable.eventdata.MapLoadingErrorEventData
+import com.mapbox.maps.extension.observable.eventdata.SourceAddedEventData
+import com.mapbox.maps.extension.observable.eventdata.StyleLoadedEventData
+import com.mapbox.maps.extension.style.expressions.generated.Expression
+import com.mapbox.maps.extension.style.layers.generated.circleLayer
+import com.mapbox.maps.extension.style.sources.generated.vectorSource
+import com.mapbox.maps.extension.style.style
+import com.mapbox.maps.plugin.delegates.listeners.OnMapLoadErrorListener
+import com.mapbox.maps.plugin.delegates.listeners.OnSourceAddedListener
+import com.mapbox.maps.plugin.delegates.listeners.OnStyleLoadedListener
 import com.mapbox.maps.plugin.gestures.gestures
 import com.mapbox.maps.plugin.locationcomponent.location
 import com.mapbox.maps.plugin.scalebar.scalebar
@@ -24,6 +33,7 @@ class MapFragment : Fragment(), LocationCallback {
 
     private lateinit var locationProvider: LocationProvider
 
+    private val styleUri = "mapbox://styles/nmondollot/cl95n147u003k15qry7pvfmq2"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,20 +48,9 @@ class MapFragment : Fragment(), LocationCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val cameraOptions = CameraOptions.Builder()
-            .center(Point.fromLngLat(2.5968216, 48.3925623))
-            .zoom(10.2)
-            .build()
-
-        binding.mapView.getMapboxMap().apply {
-            loadStyleUri("mapbox://styles/nmondollot/cl95n147u003k15qry7pvfmq2")
-            setCamera(cameraOptions)
-        }
-        binding.mapView.apply {
-            gestures.pitchEnabled = false
-            gestures.simultaneousRotateAndPinchToZoomEnabled = false
-            scalebar.enabled = false
-        }
+        setupMap()
+        addSources()
+        addLayers()
 
 
         binding.fabLocation.setOnClickListener {
@@ -60,16 +59,106 @@ class MapFragment : Fragment(), LocationCallback {
 
     }
 
+
     override fun onGPSLocation(location: Location) {
         val point = Point.fromLngLat(location.longitude, location.latitude)
 
         binding.mapView.getMapboxMap()
-            .setCamera(Builder().center(point).bearing(location.bearing.toDouble()).build())
+            .setCamera(CameraOptions.Builder().center(point).bearing(location.bearing.toDouble()).build())
         binding.mapView.gestures.focalPoint = binding.mapView.getMapboxMap().pixelForCoordinate(point)
         binding.mapView.location.updateSettings {
             enabled = true
             pulsingEnabled = true
         }
     }
+
+    private fun setupMap() {
+        val cameraOptions = CameraOptions.Builder()
+            .center(Point.fromLngLat(2.5968216, 48.3925623))
+            .zoom(10.2)
+            .build()
+
+        binding.mapView.getMapboxMap().apply {
+            loadStyleUri(styleUri)
+            setCamera(cameraOptions)
+        }
+        binding.mapView.apply {
+            gestures.pitchEnabled = false
+            gestures.simultaneousRotateAndPinchToZoomEnabled = false
+            scalebar.enabled = false
+        }
+    }
+
+    private fun addSources() {
+
+        binding.mapView.getMapboxMap().addOnSourceAddedListener(object : OnSourceAddedListener {
+            override fun onSourceAdded(eventData: SourceAddedEventData) {
+                println("MAPBOX SOURCE ADDED")
+            }
+        })
+
+        binding.mapView.getMapboxMap().addOnStyleLoadedListener(object : OnStyleLoadedListener {
+            override fun onStyleLoaded(eventData: StyleLoadedEventData) {
+                println("MAPBOX STYLE LOADED ")
+            }
+        })
+
+        binding.mapView.getMapboxMap().addOnMapLoadErrorListener(object : OnMapLoadErrorListener {
+            override fun onMapLoadError(eventData: MapLoadingErrorEventData) {
+                println("MAP STYLE SOURCE GOT ERROR ${eventData.message}")
+            }
+        }
+        )
+
+        binding.mapView.getMapboxMap().loadStyle(
+            style(styleUri) {
+                vectorSource("problems") {
+                    url("mapbox://nmondollot.4xsv235p")
+                }
+            }
+        )
+    }
+
+    private fun addLayers() {
+        val problemsSourceLayerId = "problems-ayes3a" // name of the layer in the mapbox tileset
+
+        binding.mapView.getMapboxMap().loadStyle(
+            style(styleUri) {
+                circleLayer("problems", "problems") {
+                    sourceLayer(problemsSourceLayerId)
+                    minZoom(15.0)
+                    filter(
+                        Expression.match(
+                            Expression.geometryType(),
+                            Expression.toBoolean(
+                                Expression.string()
+                            )
+                        )
+                    )
+                    circleRadius(
+                        Expression.interpolate(
+                            Expression.linear(),
+                            Expression.zoom(),
+                            Expression.abs(15.0),
+                            Expression.abs(2.0),
+                            Expression.abs(18.0),
+                            Expression.abs(4.0),
+                            Expression.abs(22.0),
+                            Expression.switchCase(
+                                Expression.boolean(
+                                    Expression.has("circuitColor"),
+                                    Expression.boolean(Expression.string())
+                                ),
+                                Expression.abs(16.0),
+                                Expression.abs(10.0)
+                            )
+                        )
+                    )
+                }
+            }
+        )
+
+    }
+
 
 }
