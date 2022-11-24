720ce67959dde0b3cd845444e2c6ee9ebb74614e
Louis Gautier <Louis.gautier@outlook.fr>
2022-11-24 13:02:36 +0100

    add variants

--- app/src/main/java/com/boolder/boolder/view/detail/ProblemBSFragment.kt
+++ app/src/main/java/com/boolder/boolder/view/detail/ProblemBSFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.PointF
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -30,6 +31,10 @@ import com.boolder.boolder.domain.model.Problem
 import com.boolder.boolder.utils.CubicCurveAlgorithm
 import com.boolder.boolder.utils.viewBinding
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.koin.android.ext.android.get
 import java.util.*
@@ -57,10 +62,16 @@ class ProblemBSFragment(private val listener: BottomSheetListener) : BottomSheet
             ?: error("No Problem in arguments")
 
     private lateinit var selectedProblem: Problem
+    private lateinit var selectedLine: Line
+    private var isVariantSelected = false
+    private val variantsCount
+        get() = completeProblem.otherCompleteProblem.count { it.problem.parentId == completeProblem.problem.id }
+
     private val bleauUrl
         get() = "https://bleau.info/a/${selectedProblem.bleauInfoId}.html"
-    private val shareurl
+    private val shareUrl
         get() = "https://www.boolder.com/${Locale.getDefault().language}/p/${selectedProblem.id}"
+    private var isVariantSelectorOpen = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(layout.bottom_sheet, container, false)
@@ -68,26 +79,82 @@ class ProblemBSFragment(private val listener: BottomSheetListener) : BottomSheet
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        selectedLine = completeProblem.line
         selectedProblem = completeProblem.problem
-
-        Glide.with(requireContext())
-            .load(completeProblem.topo?.url)
-            .placeholder(R.drawable.ic_placeholder)
-            .into(binding.picture)
-
-        setupClickEvent()
-        setupViewFor(completeProblem.problem)
-
-        drawCurves(completeProblem.line.points(), completeProblem.problem)
-        drawCircuitNumberCircle(completeProblem.line, completeProblem.problem)
-
-        completeProblem.otherCompleteProblem.map {
-            drawCircuitNumberCircle(it.line, it.problem)
+        loadBoolderImage()
+        updateLabels()
+        setupChipClick()
+        setupVariant()
+        binding.root.setOnClickListener {
+            if (isVariantSelectorOpen) {
+                closeVariantSelector()
+            }
         }
     }
 
-    private fun drawCurves(points: List<PointD>, problem: Problem) {
+    private fun markParentAsSelected() {
+        selectedLine = completeProblem.line
+        selectedProblem = completeProblem.problem
+        isVariantSelected = false
+        closeVariantSelector()
+        drawCurves()
+        drawCircuitNumberCircle()
+        updateLabels()
+    }
+
+    //region Variant(s)
+    private fun buildVariantsView() {
+        binding.variantSelector.removeAllViews()
+
+        completeProblem.otherCompleteProblem
+            .filter { it.problem.parentId == selectedProblem.id }
+            .map { variants ->
+                buildVariantText(variants.problem) { onVariantSelected(variants.line, variants.problem) }
+            }.forEach { binding.variantSelector.addView(it) }
+
+        // Add parent in variant only if a variant is currently displayed
+        if (isVariantSelected) {
+            binding.variantSelector.addView(buildVariantText(completeProblem.problem) { markParentAsSelected() }, 0)
+        }
+    }
+
+    private fun buildVariantText(problem: Problem, selectVariant: () -> Unit): TextView {
+        return TextView(requireContext()).apply {
+            text = problem.name
+            tag = problem.id
+            setTextColor(ColorStateList.valueOf(Color.BLACK))
+            setPadding(32, 16, 32, 16)
+            setOnClickListener { selectVariant() }
+        }
+    }
+
+    private fun openVariantSelection() {
+        isVariantSelectorOpen = true
+        buildVariantsView()
+        binding.variant.visibility = View.INVISIBLE
+        binding.variantSelector.visibility = View.VISIBLE
+    }
+
+    private fun closeVariantSelector() {
+        isVariantSelectorOpen = false
+        binding.variant.visibility = if (variantsCount > 0) View.VISIBLE else View.INVISIBLE
+        binding.variantSelector.visibility = View.GONE
+    }
+
+    private fun onVariantSelected(line: Line, problem: Problem) {
+        isVariantSelected = true
+        selectedLine = line
+        selectedProblem = problem
+        closeVariantSelector()
+        drawCurves()
+        drawCircuitNumberCircle()
+        updateLabels()
+    }
+    //endregion
+
+    //region Draw
+    private fun drawCurves() {
+        val points = selectedLine.points()
         if (points.isNotEmpty()) {
 
             val segment = curveAlgorithm.controlPointsFromPoints(points)
@@ -100,13 +167,13 @@ class ProblemBSFragment(private val listener: BottomSheetListener) : BottomSheet
                 points,
                 ctrl1,
                 ctrl2,
-                problem.drawColor(requireContext())
+                selectedProblem.drawColor(requireContext())
             )
         }
     }
 
-    private fun drawCircuitNumberCircle(line: Line, problem: Problem) {
-        val pointD = line.points().firstOrNull()
+    private fun drawCircuitNumberCircle() {
+        val pointD = selectedLine.points().firstOrNull()
         if (pointD != null) {
             val match = ViewGroup.LayoutParams.MATCH_PARENT
             val cardSize = 80
@@ -114,43 +181,58 @@ class ProblemBSFragment(private val listener: BottomSheetListener) : BottomSheet
             val cardParams = RelativeLayout.LayoutParams(cardSize, cardSize)
 
             val text = TextView(requireContext()).apply {
-                val textColor = if (problem.circuitColorSafe == WHITE) {
+                val textColor = if (selectedProblem.circuitColorSafe == WHITE) {
                     ColorStateList.valueOf(Color.BLACK)
                 } else ColorStateList.valueOf(Color.WHITE)
 
                 setTextColor(textColor)
                 setAutoSizeTextTypeUniformWithPresetSizes(intArrayOf(8, 10, 12), TypedValue.COMPLEX_UNIT_SP)
-                text = problem.circuitNumber
+                text = selectedProblem.circuitNumber
                 gravity = Gravity.CENTER
             }
 
             val card = CardView(requireContext())
 
             card.apply {
-                backgroundTintList = ColorStateList.valueOf(problem.drawColor(requireContext()))
-                setOnClickListener { onNewProblemSelected(line, problem) }
+                backgroundTintList = ColorStateList.valueOf(selectedProblem.drawColor(requireContext()))
                 addView(text, RelativeLayout.LayoutParams(match, match))
                 radius = 40f
-                translationX = pointD.x.toFloat() - offset
-                translationY = pointD.y.toFloat() - offset
+                translationX = ((pointD.x * binding.picture.measuredWidth) - offset).toFloat()
+                translationY = ((pointD.y * binding.picture.measuredHeight) - offset).toFloat()
             }
 
             binding.root.addView(card, cardParams)
         }
     }
+    //endregion
 
-    private fun onNewProblemSelected(line: Line, problem: Problem) {
-        selectedProblem = problem
-        setupViewFor(problem)
-        drawCurves(line.points(), problem)
-        listener.onProblemSelected(problem)
+    // region View setup
+    private fun setupVariant() {
+        binding.variant.let { chip ->
+            if (variantsCount == 0) chip.visibility = View.GONE
+            else {
+                chip.text = requireContext().resources.getQuantityString(
+                    R.plurals.variant,
+                    variantsCount,
+                    variantsCount
+                )
+                chip.visibility = View.VISIBLE
+                chip.setOnClickListener {
+                    openVariantSelection()
+                    isVariantSelectorOpen = true
+                }
+            }
+        }
     }
 
-    private fun setupViewFor(problem: Problem) {
-        binding.title.text = problem.name
-        binding.grade.text = problem.grade
+    private fun updateLabels() {
+        val sitStartText = if (selectedProblem.sitStart) {
+            requireContext().getString(R.string.sit_start)
+        } else ""
+        binding.title.text = "${selectedProblem.name} $sitStartText"
+        binding.grade.text = selectedProblem.grade
 
-        val steepnessDrawable = when (problem.steepness) {
+        val steepnessDrawable = when (selectedProblem.steepness) {
             "slab" -> R.drawable.ic_steepness_slab
             "overhang" -> R.drawable.ic_steepness_overhang
             "roof" -> R.drawable.ic_steepness_roof
@@ -161,10 +243,10 @@ class ProblemBSFragment(private val listener: BottomSheetListener) : BottomSheet
             ContextCompat.getDrawable(requireContext(), it)
         }
         binding.typeIcon.setImageDrawable(steepnessDrawable)
-        binding.typeText.text = problem.steepness.replaceFirstChar { it.uppercaseChar() }
+        binding.typeText.text = selectedProblem.steepness.replaceFirstChar { it.uppercaseChar() }
     }
 
-    private fun setupClickEvent() {
+    private fun setupChipClick() {
         binding.bleauInfo.setOnClickListener {
             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(bleauUrl))
             startActivity(browserIntent)
@@ -173,7 +255,7 @@ class ProblemBSFragment(private val listener: BottomSheetListener) : BottomSheet
         binding.share.setOnClickListener {
             val sendIntent: Intent = Intent().apply {
                 action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, shareurl)
+                putExtra(Intent.EXTRA_TEXT, shareUrl)
                 type = "text/plain"
             }
 
@@ -199,6 +281,38 @@ class ProblemBSFragment(private val listener: BottomSheetListener) : BottomSheet
 
     }
 
+    private fun loadBoolderImage() {
+        Glide.with(requireContext())
+            .load(completeProblem.topo?.url)
+            .fallback(R.drawable.ic_placeholder)
+            .addListener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressCircular.visibility = View.GONE
+                    markParentAsSelected()
+                    return false
+                }
+
+            })
+            .into(binding.picture)
+    }
+    //endregion
+
+    //region Extensions
     private fun Problem.defaultName(): String {
         return if (!circuitColor.isNullOrBlank() && !circuitNumber.isNullOrBlank()) {
             "${circuitColor.localize()} $circuitNumber"
@@ -208,6 +322,7 @@ class ProblemBSFragment(private val listener: BottomSheetListener) : BottomSheet
     private fun String.localize(): String {
         return CircuitColor.valueOf(this).localize(requireContext())
     }
+    //endregion
 }
 
 data class PointD(val x: Double, val y: Double)
