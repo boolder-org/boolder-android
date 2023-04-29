884b5c8fde3eca04836fa7472af68b9617576df1
Nicolas Mondollot <nmondollot@gmail.com>
2023-04-29 16:49:36 +0200

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.boolder.boolder"
         minSdk = 21
         targetSdk = 32
-        versionCode = 5 // bump when new version
-        versionName = "1.2.1" // bump when new version
+        versionCode = 6 // bump when new version
+        versionName = "1.3" // bump when new version
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
