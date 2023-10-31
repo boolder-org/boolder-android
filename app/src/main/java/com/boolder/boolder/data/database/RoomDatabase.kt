7f11680110268b0f6a5c6f5cde6585bdfad85ec8
Nicolas Mondollot <nmondollot@gmail.com>
2023-10-31 16:52:36 +0100

    Upgrade database to v1.3.4

--- app/src/main/java/com/boolder/boolder/data/database/RoomDatabase.kt
+++ app/src/main/java/com/boolder/boolder/data/database/RoomDatabase.kt
@@ -18,7 +18,7 @@ import com.boolder.boolder.data.database.entity.ProblemEntity
         LineEntity::class,
         ProblemEntity::class
     ],
-    version = 10, // increment version number everytime the boolder.db database changes (schema or data)
+    version = 11, // increment version number everytime the boolder.db database changes (schema or data)
     exportSchema = true
 )
 abstract class BoolderAppDatabase : RoomDatabase() {
