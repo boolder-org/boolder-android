12ed3703ec53573a1617f0ca946318934be0a627
wang-li <lechinoisdelareunion@gmail.com>
2024-02-21 23:22:15 +0100

    Fix circuit start button padding (#81)

--- app/src/main/java/com/boolder/boolder/view/map/MapFragment.kt
+++ app/src/main/java/com/boolder/boolder/view/map/MapFragment.kt
@@ -13,6 +13,9 @@ import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.postDelayed
@@ -145,6 +148,9 @@ class MapFragment : Fragment(), BoolderMapListener {
             binding.controlsOverlayComposeView.setContent {
                 BoolderTheme {
                     MapControlsOverlay(
+                        modifier = Modifier.padding(
+                            bottom = dimensionResource(id = R.dimen.height_bottom_nav_bar)
+                        ),
                         offlineAreaItem = screenState.areaState,
                         circuitState = screenState.circuitState,
                         gradeState = screenState.gradeState,
