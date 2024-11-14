c8b2666b84ca0bdc86bd93398aae8e9c5cb1716d
wang-li <lechinoisdelareunion@gmail.com>
2024-11-14 21:10:48 +0100

    Bump app version to 1.30 (#166)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.boolder.boolder"
         minSdk = 21
         targetSdk = 34
-        versionCode = 37 // bump when new version
-        versionName = "1.29.0" // bump when new version
+        versionCode = 38 // bump when new version
+        versionName = "1.30.0" // bump when new version
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
